78cd9c530e4c1dde4a99e7327cbbf20db2a5518a
Swetha <swethamagesh@elk.cse.iitb.ac.in>
2024-04-17 15:28:29 +0530

    image to image

--- edit2.py
+++ edit2.py
@@ -14,10 +14,12 @@ pipe = pipe.to(device)
 # response = requests.get(url)
 # init_image = Image.open(BytesIO(response.content)).convert("RGB")
 
-init_image = Image.open("/mnt/nas/swethamagesh/emotion/PiedPiper/iamge1.jpg")
-init_image = init_image.resize((768, 512))
+init_image = Image.open("/mnt/nas/swethamagesh/emotion/Asyrp_official/test_images/afhq/contents/flickr_dog_000772.png")
+# init_image = init_image.resize((768, 512))
+init_image.thumbnail((512, 512))
 
-prompt = "Make the boy happier"
-
+prompt = "a cartoon of a dog with a happy expression"
+# 
+# images = pipe(prompt=prompt, image=init_image).images
 images = pipe(prompt=prompt, image=init_image, strength=0.75, guidance_scale=7.5).images
 images[0].save("/mnt/nas/swethamagesh/emotion/PiedPiper/editted_image.png")
